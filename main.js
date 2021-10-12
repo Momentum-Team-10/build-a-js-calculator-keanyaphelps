f48ca6311bbcb9765cf0eae269de794c32a46282
keanya phelps <keanyaphelps@keanyas-MacBook-Air.local>
2021-10-11 19:49:44 -0500

    cleanup

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-let buttons = document.querySelectorAll(".number-button")
-console.log(buttons)
-for (let button of buttons){
+let display = document.getElementById("display");
+console.log("test");
+let buttons = document.querySelectorAll(".number-button");
+console.log("test1");
+for (let button of buttons) {
+    console.log(button);
     button.addEventListener("click", () => {
-        display.innetText += button.innertext
-    })
-    
+        console.log(display.innerText);
+        display.innerText += button.innerText;
+    });
+
 }
-let equals = document.getElementById('equals')
+let equals = document.getElementById('equals');
 
-equals.addEventListener('Click', () => {
-    display.innetText = eval(display.innetText)
+equals.addEventListener("click", () => {
+    display.innerText = eval(display.innerText);
+});
+let clear = document.getElementById("bclear");
 
-})
+clear.addEventListener ("click", () => {
+    display.innerText = ""
+});
